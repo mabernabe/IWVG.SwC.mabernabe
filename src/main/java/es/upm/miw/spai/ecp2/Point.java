7b65f0d7e123080d80ccc3cd042c9d5c3a60738b
Alan Mark <am.sousa@alumnos.upm.es>
2016-10-03 21:30:17 +0200

    Completed Point Class Enhancements and tests

--- src/main/java/es/upm/miw/spai/ecp2/Point.java
+++ src/main/java/es/upm/miw/spai/ecp2/Point.java
@@ -1,19 +1,20 @@
 package es.upm.miw.spai.ecp2;
 
 public class Point {
-    private int x, y;
+    private int x, y, z;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
-    public Point(int xy) {
-        this(xy, xy);
+    public Point(int xyz) {
+        this(xyz, xyz, xyz);
     }
 
     public Point() {
-        this(0, 0);
+        this(0, 0, 0);
     }
 
     public int getX() {
@@ -24,22 +25,35 @@ public class Point {
         return this.y;
     }
 
-    public double module() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+    public int getZ() {
+        return this.z;
     }
 
-    public double phase() {
+    public double module() {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    public double phaseXY() {
         return Math.atan((double) this.y / this.x);
+    }
+
+    public double phaseZX() {
+        return Math.atan((double) this.x / this.z);
+    }
+
+    public double phaseYZ() {
+        return Math.atan((double) this.z / this.y);
     }
 
     public void translateOrigin(Point origin) {
         this.x -= origin.getX();
         this.y -= origin.getY();
+        this.z -= origin.getZ();
     }
 
     @Override
     public String toString() {
-        return "Point[" + x + "," + y + "]";
+        return "Point[" + x + "," + y + "," + z + "]";
     }
     
 }
